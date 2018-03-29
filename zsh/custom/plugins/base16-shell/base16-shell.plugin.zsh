018a52ced9340b5676566b11d0c1c302dddea525
Francis Vidal <francis.vidal@gmail.com>
2018-03-29 16:42:19 +0800

    Fixed base16-shell.plugin.zsh path

--- zsh/custom/plugins/base16-shell/base16-shell.plugin.zsh
+++ zsh/custom/plugins/base16-shell/base16-shell.plugin.zsh
@@ -1,1 +1,2 @@
-/home/francisv/.dotfiles/base16-shell/base16-shell.plugin.zsh
+BASE16_SHELL="$HOME/.dotfiles/base16-shell"
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
